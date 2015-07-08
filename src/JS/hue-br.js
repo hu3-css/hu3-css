36e6011d6462e0e13fb3e23bc64e5c7c4f90a0ec
Felype Rennan <felyperennan@hotmail.com>
2015-07-08 10:56:35 -0300

    Adicionando alguns componentes

---
+++ src/JS/hue-br.js
@@ -0,0 +1,28 @@
+﻿
+$(document).ready(function() {
+    // HEROI
+    if ($(".heroi").first()) {
+        if ($(".nav.extravagante").first()) {
+            $(".nav.extravagante").addClass("transparente");
+        }
+    }
+    $(".heroi").each(function () {
+        $(this).css("background-image", "url('"+$(this).attr("data-imagem")+"')");
+    });
+    // -----
+    // MENU-ATRAS
+    $(".menu-atras").find(".abrir-menu").each(function() {
+        $(this).click(function(e) {
+            var v = $(this).parent();
+            while(v !== undefined) {
+                console.log('aa');
+                if(v.hasClass("menu-atras")) {
+                    v.toggleClass("abrir");
+                    break;                
+                }
+                v = v.parent();
+            }
+        });
+    });
+    // -----
+});
